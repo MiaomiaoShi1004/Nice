35466a5790e4da442cb6252b56e7655ea4782f3f
Miaomiao Shi <simoneshi1004@outlook.com>
2023-10-03 04:23:59 +0100

    "Add TestingPage to App.js"

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Nice
+
+# To run this
+cd src
+npx expo start
+
+# To test new features in TestPage
--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import TestScreen from './screens/TestScreen';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    
     <TestScreen/>
   );
 }
